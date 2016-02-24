7ca8cd77361840cb5f257462ca75c7fef2c5e506
Paul M. Christian <paulmchristian@gmail.com>
2016-02-24 10:47:53 -0600

    enable LAN connections to dev server

--- devServer.js
+++ devServer.js
@@ -17,11 +17,30 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, 'localhost', function(err) {
+app.listen(8080, '0.0.0.0', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:3000');
+  console.log('Listening at http://localhost:8080');
+  console.log('IP addresses: ', getIPAddresses());
 });
+
+function getIPAddresses() {
+  var os = require("os"),
+      interfaces = os.networkInterfaces(),
+      ipAddresses = [];
+
+  for (var deviceName in interfaces) {
+    var addresses = interfaces[deviceName];
+    for (var i = 0; i < addresses.length; i++) {
+      var addressInfo = addresses[i];
+      if (addressInfo.family === "IPv4" && !addressInfo.internal) {
+        ipAddresses.push(addressInfo.address);
+      }
+    }
+  }
+
+  return ipAddresses;
+}
