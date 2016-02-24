d2431686185c23e700e5ed0d5f4a50dd4cd9b2e6
Paul M. Christian <paulmchristian@gmail.com>
2016-02-24 11:51:00 -0600

    add ghpages; app in src and static directories

--- devServer.js
+++ devServer.js
@@ -6,6 +6,8 @@ var config = require('./webpack.config.dev');
 var app = express();
 var compiler = webpack(config);
 
+app.use(express.static(__dirname + '/static'));
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
@@ -14,7 +16,7 @@ app.use(require('webpack-dev-middleware')(compiler, {
 app.use(require('webpack-hot-middleware')(compiler));
 
 app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'static/index.html'));
 });
 
 app.listen(8080, '0.0.0.0', function(err) {
--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ export class App extends Component {
   render() {
     return (
       <div>
+        <img src="images/test.jpg" alt=""/>
         <Counter increment={1} color={NICE} />
         <Counter increment={5} color={SUPER_NICE} />
       </div>
