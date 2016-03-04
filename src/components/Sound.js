c92bd0645fb0681ff81bb12b3e99dc9ec95b2279
Paul M. Christian <paulmchristian@gmail.com>
2016-03-03 20:49:11 -0600

    add Sound component

---
+++ src/components/Sound.js
@@ -0,0 +1,39 @@
+import {Entity} from 'aframe-react';
+import React from 'react';
+
+class Sound extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    if(!this.props.isPlaying && nextProps.isPlaying) {
+      this.sound && this.sound.play();
+    }
+  }
+
+  render() {
+    const onLoaded = evt => {
+      const el = evt.target;
+      this.sound = el.components.sound;
+
+      if(this.props.isPlaying){
+        this.sound.play();
+      }
+
+      el.addEventListener('onended', () => {
+        this.props.onEnded();
+        this.isPlaying = false;
+      });
+    };
+
+    return (
+      <Entity {...this.props}
+        sound={{src: this.props.src, autoplay: false}}
+        onLoaded={onLoaded}
+      />
+    )
+  }
+}
+
+Sound.defaultProps = {
+  onEnded: () => {},
+};
+
+export default Sound;
