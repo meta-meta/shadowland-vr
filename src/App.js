85f759ab370e2c4d0693c8afa54fc37ad3ad5d2f
Paul M. Christian <paulmchristian@gmail.com>
2016-03-07 09:55:11 -0600

    position Leap Motion hands relative to camera and orient them depending on VR or non-VR

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Camera from './components/Camera';
 import Image from './components/Image';
 import Sky from './components/Sky';
 import Text from './components/Text';
-import Box from './components/Box';
-import {hsvToHex} from './util/colorConversion';
+import Hand from './components/Hand';
 
 import _ from 'lodash';
+import 'leapjs-plugins';
 import Leap from 'leapjs';
 
 export class App extends Component {
@@ -18,8 +18,10 @@ export class App extends Component {
     super();
 
     this.state = {
+      isVR: false,
       leapFrame: undefined,
       left: {
+        confidence: 0,
         fingers: [],
         palm: V3(),
         pitchYawRoll: V3(),
@@ -27,6 +29,7 @@ export class App extends Component {
         pinchStrength: 0,
       },
       right: {
+        confidence: 0,
         fingers: [],
         palm: V3(),
         pitchYawRoll: V3(),
@@ -36,97 +39,44 @@ export class App extends Component {
     };
 
     Leap.loop({background: true}, (frame) => {
-      if (frame.hands.length) {
-        //debugger;
-      }
-      _.each(frame.hands, hand => this.setState({
-        [hand.type]: {
-          fingers: hand.fingers.map(finger => arrToV3(finger.tipPosition).divideScalar(1000)),
-          palm: arrToV3(hand.palmPosition).divideScalar(1000),
-          pitchYawRoll: V3(hand.pitch(), -hand.yaw(), hand.roll()).multiplyScalar(180 / Math.PI),
-          pinchStrength: hand.pinchStrength,
-          grabStrength: hand.grabStrength,
+        if (frame.hands.length) {
+          //debugger;
         }
-      }));
-    });
-    //  .use('handEntry')
-    //.on('handFound', (hand) => {
-    //
-    //})
+        _.each(frame.hands, hand => this.setState({
+          [hand.type]: {
+            confidence: hand.confidence,
+            fingers: hand.fingers.map(finger => arrToV3(finger.tipPosition).divideScalar(1000)),
+            palm: arrToV3(hand.palmPosition).divideScalar(1000),
+            pitchYawRoll: V3(hand.pitch(), -hand.yaw(), hand.roll()).multiplyScalar(180 / Math.PI),
+            pinchStrength: hand.pinchStrength,
+            grabStrength: hand.grabStrength,
+          }
+        }));
+      })
+      .use('handEntry')
+      .on('handFound', (hand) => {
+      })
+      .on('handLost', (hand) => {
+        this.setState({[hand.type]: _.extend(this.state[hand.type], {confidence: 0})});
+      })
 
   }
 
   render() {
     const {left, right} = this.state;
 
-    const hand = (hand) => <Entity >
-      {
-        hand.fingers.map(
-          (finger, i) => <Box key={i}
-                              color={hsvToHex(i / 5, 1, i < 2 ? 0.5 + hand.pinchStrength / 2 : 0.5 )}
-                              width={0.01}
-                              height={0.01}
-                              depth={0.01}
-                              position={finger.toAframeString()}
-          />)
-      }
-      <Box color={hsvToHex(0, 0, 0.5)}
-           width={0.01}
-           height={0.01}
-           depth={0.01}
-           position={hand.palm.toAframeString()}
-           scale={V3(1, 1, 1).add(V3(1, 1, 1).multiplyScalar(hand.grabStrength)).toAframeString()}
-      />
-      {
-        /*<Entity position={hand.palm.toAframeString()}
-         rotation={hand.pitchYawRoll.toAframeString()}
-         ref={(cmp)=>{
-         //const el = ReactDOM.findDOMNode(cmp);
-         //console.log(el && el.object3D)
-         }}
-         >
-         <Box color={hsvToHex(0, 1, 0.5)}
-         width={0.02}
-         height={0.01}
-         depth={0.02}
-         position="-0.01 0 0.01"
-         />
-
-         <Box color={hsvToHex(0.2, 1, 0.5)}
-         width={0.02}
-         height={0.01}
-         depth={0.02}
-         position="0.01 0 0.01"
-         />
-
-         <Box color={hsvToHex(0.4, 1, 0.5)}
-         width={0.02}
-         height={0.01}
-         depth={0.02}
-         position="-0.01 0 -0.01"
-         />
-
-         <Box color={hsvToHex(0.8, 1, 0.5)}
-         width={0.02}
-         height={0.01}
-         depth={0.02}
-         position="0.01 0 -0.01"
-         />
-         </Entity>*/
-      }
-
-    </Entity>;
-
-
     return (
-      <Scene>
-        <Camera/>
+      <Scene onEnterVR={() => {this.setState({isVR: true});}}
+             onExitVR={() => {this.setState({isVR: false});}}
+      >
+        <Camera>
+          <Entity rotation={this.state.isVR ? "90 0 180" : "0 0 0"}
+                  position={this.state.isVR ? "0 0 0" : "0 -0.2 -0.7"}>
+            <Hand hand={left}/>
+            <Hand hand={right}/>
+          </Entity>
+        </Camera>
         <Sky/>
-
-        <Entity position="0 -0.2 -0.2">
-          {hand(left)}
-          {hand(right)}
-        </Entity>
 
       </Scene>
     );
